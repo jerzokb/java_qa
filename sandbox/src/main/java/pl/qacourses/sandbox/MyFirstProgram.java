db2e6fe8c6d5b0accccdfc91149d65087dc492ba
Beata Jerzok-Palka <beata@palkabm.net>
2020-05-10 20:08:33 +0200

    Funkcje

--- sandbox/src/main/java/pl/qacourses/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/qacourses/sandbox/MyFirstProgram.java
@@ -4,7 +4,27 @@ public class MyFirstProgram {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("Beata");
+
+		double len = 5;
+		System.out.println("Powierzchnia kwadratu o boku " + len + " = " + area(len));
+
+		double a = 4;
+		double b = 5;
+		System.out.println("Powierzchnia prostokata o bokach " + a + " i " + b + " = " + area(len));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello " + somebody);
+	}
+
+	public static double area(double l) {
+		return l*l;
+	}
+
+	public static double area(double a, double b) {
+		return a*b;
 	}
 
 }
