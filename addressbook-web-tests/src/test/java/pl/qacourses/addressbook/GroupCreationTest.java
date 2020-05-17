ba6be1b9b27eea0579a85d0cca429754abbec1be
Beata Jerzok-Palka <beata@palkabm.net>
2020-05-17 23:11:30 +0200

    Add New Contact Test

--- addressbook-web-tests/src/test/java/pl/qacourses/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/qacourses/addressbook/GroupCreationTest.java
@@ -68,8 +68,6 @@ public class GroupCreationTest {
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.findElement(By.linkText("Logout")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
     wd.quit();
   }
 
