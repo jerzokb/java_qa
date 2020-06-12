dcb443e4d1e25df512de511295c6fb5cb10b7b48
Beata Jerzok-Palka <beata@palkabm.net>
2020-06-12 22:59:26 +0200

    Module 6 (5-11)

--- addressbook-web-tests/src/test/java/pl/qacourses/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/qacourses/addressbook/appmanager/ApplicationManager.java
@@ -8,9 +8,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private final Properties properties;
     WebDriver wd;
 
     private ContactNavigationHelper contactNavigationHelper;
@@ -20,11 +26,16 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser)  {
+
         this.browser = browser;
+        properties = new Properties();
+
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
@@ -35,13 +46,15 @@ public class ApplicationManager {
         }
 
         wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+        //wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
         contactNavigationHelper = new ContactNavigationHelper(wd);
-        sessionHelper.login("admin", "secret");
+        //sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- addressbook-web-tests/src/test/java/pl/qacourses/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/pl/qacourses/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,9 @@ package pl.qacourses.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
 import pl.qacourses.addressbook.model.GroupData;
 
 import java.io.File;
@@ -19,6 +22,9 @@ public class GroupDataGenerator {
 
     @Parameter(names = "-f", description = "Target file")
     public String file;
+
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
 
     public static void main(String[] args) throws IOException {
         GroupDataGenerator generator = new GroupDataGenerator();
@@ -38,16 +44,52 @@ public class GroupDataGenerator {
 
     private void run() throws IOException {
         List<GroupData> groups = generateGroups(count);
+        if (format.equals("csv")) {
+            saveAsCSV(groups, new File(file));
+        } else if(format.equals("xml")) {
+            saveAsXML(groups, new File(file));
+        } else if(format.equals("json")) {
+            saveAsJson(groups, new File(file));
+        } else {
+            System.out.println("Unrecognized file format" + format);
+        }
 
-        save(groups, new File(file));
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
-        Writer writer = new FileWriter(file);
-        for (GroupData group : groups) {
-            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
         }
-        writer.close();
+        //Writer writer = new FileWriter(file);
+        //writer.write(json);
+        //writer.close();
+    }
+
+    private void saveAsXML(List<GroupData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        //xstream.alias("group",GroupData.class);
+        xstream.processAnnotations(GroupData.class);
+        String xml = xstream.toXML(groups);
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
+        //Writer writer = new FileWriter(file);
+        //writer.write(xml);
+        //writer.close();
+    }
+
+    private void saveAsCSV(List<GroupData> groups, File file) throws IOException {
+        try (Writer writer = new FileWriter(file)) {
+            for (GroupData group : groups) {
+                writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+            }
+        }
+        //for (GroupData group : groups) {
+        //    writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+        //}
+        //writer.close();
     }
 
     private List<GroupData> generateGroups(int count) {
